5f327b07696e0a9886c0d6e062da0620440e6341
wnnabjt <2016586625@qq.com>
2020-06-26 00:28:56 +0800

    some training code

--- league_a/table_tennis.cpp
+++ league_a/table_tennis.cpp
@@ -1,136 +1,117 @@
-/*
-    本题大意：共有n个可用的桌子，当有一对运动员到来时，如果有桌子是可用的，
-    则选取桌子编号最小的那个桌子比赛，否则他们将加入一个队列。
-    你的任务是计算每组运队员在队列中等待时间以及每个桌子招待的组数。
-    一些关于vip问题使这个程序变得复杂： ①，当一个vip桌子可用，队列中第一
-    组vip使用该桌子。②，队列中没有vip时，vip桌子可以被!vip使用。
-    ③，如果有一对vip到了，但是并没有可用的vip桌子，那么vip可以使用普通桌子。
-
-    input:
-        n(运动员组数)
-        1 ~ n 每个运动员的到达时间  比赛时间  是否会员标志位
-        k(number of players) m(number of vip)
-        1 ~ m vip桌子的编号
-    
-    output:
-        首先按照服务时间序输出参赛运动员的信息，包括 到达时间 服务时间 等待时间 
-        如若有一队未在关店时间之前参加比赛，则不输出其信息。
-        然后在一行输出每张桌子的服务人数。
-
-
-    本题思路：将所有players按照到达时间序排序，把所有vip_players按照到达
-    时间序排序。遍历所有桌子，寻找第一个服务结束且编号最靠前的桌子，如果该桌子
-    为!vip桌子，则服务目前第一对players，如果为vip桌子，则服务第一对vip_players
-    ，如果无vip则服务第一对players。
-*/
-
-#include <cstdio>
+#include <iostream>
 #include <string>
+#include <map>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 using namespace std;
 
-struct Players {
-    int hours, minutes, seconds, last_time, is_vip, wait_time, id;
-    int s_hours, s_minutes, s_seconds;
+struct A {
+    string arr, serve;
+    int wait_time;
 };
 
-vector <Players> all_players, vip_players;
+struct B {
+    int arr_time, last_time;
+};
 
-const int maxn = 100 + 5, maxm = 10000 + 5, inf = 0x3f3f3f3f;
+map <string, B> mp, mp_vip;
+vector <int> vec(100 + 5), num(100 + 5);
+vector <bool> table_vip(100 + 5);
+vector <A> ans;
 
-bool table[maxn];
-bool vis[maxm];
-int num[maxn];
-int end_time[maxn];
-
-bool cmp(const Players a, const Players b) {
-    return a.hours * 3600 + a.minutes * 60 + a.seconds < b.hours * 3600 + b.minutes * 60 + b.seconds;
+int get_time(string time) {
+    char c;
+    int h, m, s;
+    stringstream ss;
+    ss << time;
+    ss >> h >> c >> m >> c >> s;
+    return h * 3600 + m * 60 + s;
 }
 
-int Time(const Players a) {
-    return a.hours * 3600 + a.minutes * 60 + a.seconds;
+string get_serve(int time) {
+    string ss;
+    int h, m, s;
+    h = time / 3600;
+    m = (time % 3600) / 60;
+    s = (time % 3600) % 60;
+    if(h < 10) ss += "0";
+    ss += to_string(h);
+    ss += ":";
+    if(m < 10) ss += "0";
+    ss += to_string(m);
+    ss += ":";
+    if(s < 10) ss += "0";
+    ss += to_string(s);
+    return ss;
 }
 
 int main() {
-    for(int i = 1; i < maxn; i ++) {
-        end_time[i] = 8 * 60 * 60;
-    }
-    int n, h, m, s, last_time, flag_vip;
-    scanf("%d", &n);
-    for(int i = 1; i <= n; i ++) {
-        scanf("%d:%d:%d %d %d", &h, &m, &s, &last_time, &flag_vip);
+    string time;
+    int n, k, m, last_time, is_vip, arr_time;
+    cin >> n;
+    while(n --) {
+        cin >> time >> last_time >> is_vip;
+        arr_time = get_time(time);
         if(last_time > 120) last_time = 120;
-        all_players.push_back(Players{h, m, s, last_time, flag_vip, 0, i, 0, 0, 0});
-        if(flag_vip) vip_players.push_back(Players{h, m, s, last_time, flag_vip, 0, i, 0, 0, 0});
+        mp[time] = B{arr_time, last_time};
+        if(is_vip) {
+            mp_vip[time] = B{arr_time, last_time};
+        }
     }
-    int K, M, vip_table;
-    scanf("%d %d", &K, &M);
-    for(int i = 0; i < M; i ++) {
-        scanf("%d", &vip_table);
-        table[vip_table] = true;
+    cin >> k >> m;
+    for(int i = 1; i <= k; i ++) {
+        vec[i] = 8 * 3600;
+        table_vip[i] = false;
+        num[i] = 0;
     }
-    sort(all_players.begin(), all_players.end(), cmp);
-    sort(vip_players.begin(), vip_players.end(), cmp);
-    int pos_all = 0, pos_vip = 0;
+    while(m --) {
+        cin >> is_vip;
+        table_vip[is_vip] = true;
+    }
+    map <string, B> :: iterator it;
     while(true) {
-        if(pos_all >= n) break;
-        int k = 0, min_elem = inf;
-        for(int i = 1; i <= K; i ++) {
-            if(min_elem > end_time[i]) {
-                min_elem = end_time[i];
-                k = i;
+        if(mp.empty() || mp.begin() -> first >= "21:00:00") break;
+        int min_time = *min_element(vec.begin() + 1, vec.begin() + k + 1), pos;
+        for(int i = 1; i <= k; i ++) {
+            if(vec[i] == min_time) {
+                pos = i;
+                break;
             }
         }
-        // printf("is vip ? %d\n", table[k]);
-        // printf("The first is %d -> %d:%d:%d\n", k, min_elem / 3600, min_elem % 3600 / 60, min_elem % 3600 % 60);
-        if(min_elem >= 21 * 3600) break;
-        if(Time(vip_players[pos_vip]) <= min_elem && table[k]) { //队列中有vip，且最先好的桌子是vip桌子
-            if(vis[vip_players[pos_vip].id]) {
-                pos_vip++;
-                continue;
+        if(vec[pos] >= 21 * 3600) break;
+        string serve;
+        if(table_vip[pos] && !mp_vip.empty() && mp_vip.begin() -> second.arr_time <= vec[pos]) {
+            num[pos] ++;
+            serve = get_serve(vec[pos]);
+            ans.push_back(A{mp_vip.begin() -> first, serve, (vec[pos] - mp_vip.begin() -> second.arr_time + 30) / 60});
+            vec[pos] += 60 * mp_vip.begin() -> second.last_time;
+            it = mp.find(mp_vip.begin() -> first);
+            if(it != mp.end()) {
+                mp.erase(it);
             }
-            vis[vip_players[pos_vip].id] = true;
-            // printf("It's be true %d\n", vis[vip_players[pos_vip].id]);
-            Players &temp = vip_players[pos_vip];
-            num[k] ++;
-            int T = Time(vip_players[pos_vip]);
-            end_time[k] = max(T, min_elem) + temp.last_time * 60;
-            temp.s_hours = max(T, min_elem) / 3600;
-            temp.s_minutes = (max(T, min_elem) % 3600) / 60;
-            temp.s_seconds = (max(T, min_elem) % 3600) % 60;
-            if(T < min_elem) {
-                temp.wait_time = (min_elem - T) / 60;
-                if((min_elem - T) % 3600 % 60 >= 30) temp.wait_time ++;
-            }
-            pos_vip ++;
-            end_time[k] = max(T, min_elem) + temp.last_time * 60;
-            printf("%02d:%02d:%02d %02d:%02d:%02d %d\n",temp.hours, temp.minutes, temp.seconds, temp.s_hours, temp.s_minutes, temp.s_seconds, temp.wait_time);
+            it = mp_vip.find(mp_vip.begin() -> first);
+            mp_vip.erase(it);
         } else {
-            if(vis[all_players[pos_all].id]) {
-                pos_all ++;
-                continue;
+            num[pos] ++;
+            serve = get_serve(max(vec[pos], mp.begin() -> second.arr_time));
+            ans.push_back(A{mp.begin() -> first, serve, vec[pos] > mp.begin() -> second.arr_time ? ((vec[pos] - mp.begin() -> second.arr_time) + 30) / 60 : 0 });
+            vec[pos] = max(vec[pos], mp.begin() -> second.arr_time) + 60 * mp.begin() -> second.last_time;
+            it = mp.find(mp.begin() -> first);
+            mp.erase(it);
+            it = mp_vip.find(mp.begin() -> first);
+            if(it != mp_vip.end()) {
+                mp_vip.erase(it);
             }
-            vis[all_players[pos_all].id] = true;
-            // printf("It's be true %d\n", vis[all_players[pos_all].id]);
-            Players &temp = all_players[pos_all];
-            num[k] ++;
-            int T = Time(all_players[pos_all]);
-            end_time[k] = max(T, min_elem) + temp.last_time * 60;
-            temp.s_hours = max(T, min_elem) / 3600;
-            temp.s_minutes = (max(T, min_elem) % 3600) / 60;
-            temp.s_seconds = (max(T, min_elem) % 3600) % 60;
-            if(T < min_elem) {
-                temp.wait_time = (min_elem - T) / 60;
-                if((min_elem - T) % 3600 % 60 >= 30) temp.wait_time ++;
-            }
-            printf("%02d:%02d:%02d %02d:%02d:%02d %d\n",temp.hours, temp.minutes, temp.seconds, temp.s_hours, temp.s_minutes, temp.s_seconds, temp.wait_time);
-            pos_all ++;
         }
     }
-    for(int i = 1; i <= K; i ++) {
-        if(i > 1) printf(" ");
-        printf("%d", num[i]);
+    for(int i = 0; i < ans.size(); i ++) {
+        cout << ans[i].arr << ' ' << ans[i].serve << ' ' << ans[i].wait_time << endl;
     }
+    for(int i = 1; i <= k; i ++) {
+        if(i ^ 1) cout << ' ';
+        cout << num[i];
+    }
+    cout << endl;
     return 0;
 }
